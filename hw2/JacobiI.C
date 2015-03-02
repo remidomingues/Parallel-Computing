9ad0027490eef75c77019471a47d86bdc9c1d54b
JWARKTH <jwar@kth.se>
2015-03-02 12:13:20 +0100

    Update JacobiI.C
    Commented some more

--- hw2/JacobiI.C
+++ hw2/JacobiI.C
@@ -47,7 +47,7 @@ int 	k;
     int step;
 for (step = 0; step < SMX; step++) {
 /* RB communication of overlap */
-	if(p==0){ // First is red
+	if(p==0){ // First is red and contains left boundary 
 		MPI_Send( (void *) (&(u[I-2])), 1, MPI_DOUBLE, p+1,1, MPI_COMM_WORLD );
 		MPI_Recv( (&(u[I-1])),  1, MPI_DOUBLE, p+1,1, MPI_COMM_WORLD,MPI_STATUS_IGNORE );
 		u[0] = 0;
@@ -67,7 +67,7 @@ for (step = 0; step < SMX; step++) {
 		MPI_Send((void *) (&(u[1])), 1, MPI_DOUBLE, p-1,1, MPI_COMM_WORLD );	
 		MPI_Recv((&(u[0])), 1, MPI_DOUBLE, p-1,1, MPI_COMM_WORLD,MPI_STATUS_IGNORE );
 	}
-	else{
+	else{  //Last block contains right boundary condition
 		MPI_Recv(( &(u[0])),  1, MPI_DOUBLE, p-1,1, MPI_COMM_WORLD,MPI_STATUS_IGNORE );
 		MPI_Send((void *) (&(u[1])), 1, MPI_DOUBLE, p-1,1, MPI_COMM_WORLD);
 		u[I-1]=0;
