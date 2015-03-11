d6223ffd6ab99a36d76aa66d65e5a9bc40937e9b
Rémi Domingues <remidomingues@live.fr>
2015-03-11 23:00:05 +0100

    Bug fix in sequential sort and data distribution

--- hw3/oddevensort.c
+++ hw3/oddevensort.c
@@ -5,14 +5,17 @@
 void display(double* array, int length, int rank) {
     int i;
     for(i = 0; i < length; ++i) {
-        printf("%d > %.2f ", rank, array[i]);
+        printf("%.2f ", array[i]);
     }
     printf("\n");
 }
 
-int compare(const void * a, const void * b)
+int compare(const void *x, const void *y)
 {
-  return ( *(double*)a - *(double*)b );
+  double xx = *(double*)x, yy = *(double*)y;
+  if (xx < yy) return -1;
+  if (xx > yy) return  1;
+  return 0;
 }
 
 double* merge_split(double* array, double* tmp, int length, int higher) {
@@ -32,16 +35,16 @@ double* merge_split(double* array, double* tmp, int length, int higher) {
     }
 }
 
-double* exchange(double* array, int length, int rank, int sendFirst) {
+double* exchange(double* array, int length, int length2, int rank, int sendFirst) {
     double* tmp = (double*)malloc(length * sizeof(double));
     double* result;
 
     if(sendFirst == 1) {
         MPI_Send(array, length, MPI_DOUBLE, rank, 1, MPI_COMM_WORLD);
-        MPI_Recv(tmp, length, MPI_DOUBLE, rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(tmp, length2, MPI_DOUBLE, rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         result = merge_split(array, tmp, length, 0);
     } else {
-        MPI_Recv(tmp, length, MPI_DOUBLE, rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(tmp, length2, MPI_DOUBLE, rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Send(array, length, MPI_DOUBLE, rank, 1, MPI_COMM_WORLD);
         result = merge_split(array, tmp, length, 1);
     }
@@ -52,7 +55,7 @@ double* exchange(double* array, int length, int rank, int sendFirst) {
 
 int main(int argc, char **argv)
 {
-    int i, step;
+    int i, j, step, I2;
     unsigned int P;
     unsigned int rank;
 
@@ -66,8 +69,13 @@ int main(int argc, char **argv)
         exit(-1);
     }
     int N = atoi(argv[1]);
-    /* local size. Modify if P does not divide N */
-    int I = N/P;
+    /* local size */
+    int R = N%P;
+    int I = (N-R)/P; //Number of local elements common to all
+    if(R - rank > 0) {
+        ++I;
+    }
+
     double* x = (double*)malloc(I * sizeof(double));
     /* Initialization of the seed for the random generator */
     srandom(rank+1);
@@ -81,7 +89,7 @@ int main(int argc, char **argv)
     unsigned int evenprocess = (i+1) % 2;
     unsigned int evenphase = 1;
 
-    for(step = 0; step < N-1; ++step) {
+    for(step = 0; step < P; ++step) {
         printf("%d > Internal sort:", rank);
         display(x, I*P, rank);
 
@@ -93,11 +101,19 @@ int main(int argc, char **argv)
 
         if((evenphase == 1 && evenprocess == 1) || (evenphase == 0 && evenprocess == 0)) {
             if(rank < N-1) {
-                exchange(x, I, rank+1, 1);
+                I2 = I;
+                if(R != 0 && ((R - rank <= 0) != (R - rank + 1 <= 0))) {
+                    I2 = I - 1;
+                }
+                exchange(x, I, I2, rank+1, 1);
             }
         } else {
             if(rank > 0) {
-                exchange(x, I, rank-1, 0);
+                I2 = I;
+                if(R != 0 && ((R - rank <= 0) != (R - rank - 1 <= 0))) {
+                    I2 = I + 1;
+                }
+                exchange(x, I, I2, rank-1, 0);
             }
         }
 
@@ -105,15 +121,39 @@ int main(int argc, char **argv)
     }
 
     // Data gathering
-    double* data;
+    double** data;
     if(rank == 0) {
-        data = (double *)malloc(P * I * sizeof(double));
+        data = (double **)malloc(P * sizeof(double*));
     }
 
-    MPI_Gather(x, I, MPI_DOUBLE, data, I, MPI_DOUBLE, 1, MPI_COMM_WORLD);
-
-    //TODO REMOVE
     if(rank == 0) {
-        display(data, I*P, rank);
+        for(i = 1; i < P; ++i) {
+            if(R != 0 && R - i <= 0) {
+                data[i] = (double *)malloc((I-1) * sizeof(double));
+                MPI_Recv(data[i], I-1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            } else {
+                data[i] = (double *)malloc(I * sizeof(double));
+                MPI_Recv(data[i], I, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+        }
+    } else {
+        MPI_Send(x, I, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+    }
+
+    //Final display
+    if(rank == 0) {
+        printf("%d > Final result\n", 0);
+        for(i = 0; i < P; ++i) {
+            if(R != 0 && R - i <= 0) {
+                step = I - 1;
+            } else {
+                step = I;
+            }
+
+            for(j = 0; j < step; j++) {
+                printf("%.2f ", data[i][j]);
+            }
+        }
+        printf("\n");
     }
 }
